e5751bfe6031a04b1a26ad036bf036064474eddd
VeinQueen <fengfulee@163.com>
2014-07-29 09:26:32 +0800

    PCRE----Perl Compatible Regular Expression

--- PCRETest.php
+++ PCRETest.php
@@ -25,6 +25,66 @@ class PCRETest extends PHPUnit_Framework_TestCase {
 		//there are 3 space in the first chars.
 	 
 
+		$chars = preg_split('/ /', $str, -1, PREG_SPLIT_OFFSET_CAPTURE);
+		//print_r($chars);
+		//
+		
+	}
+
+	public function test_preg_replace () 	{
+		//first test , preg_replace can do what str_replace can do .
+		$subject = "aa-bb-cc-dd";
+		$search = array('a', 'b', 'c');
+		$replace = array('A', 'B', 'C');
+		$ret = str_replace($search, $replace, $subject);
+		$this->assertEquals('AA-BB-CC-dd',$ret);
+		
+		//second test.
+		$search = '/-/';	//here , you should add forward slash on both sides of the string.
+		$replace = '#';
+		$ret = preg_replace($search, $replace, $subject);
+
+		$this->expectOutputString ('aa#bb#cc#dd');
+		print $ret;
+ 
+	}
+
+	public function test_preg_match_all () {
+		$subject =<<<END
+		<a class="class1">www.baidu.com</a>
+		<a class="class1">www.google.com</a>
+		<a class="class1">www.bing.com</a>
+END;
+		$pattern ='/\<a class="class1"\>(\w.*?)\<\/a\>/';
+		preg_match_all($pattern, $subject, $matches);
+		
+		$arr = array(
+			'www.baidu.com',
+			'www.google.com',
+			'www.bing.com'
+			);
+
+		$this->assertEquals($arr,$matches[1]);
+		
+
+	}
+
+	public function test_preg_match () {
+		$pattern = '@^(?:http://)?([^/]+)@i';
+		$subject = "http://www.php.net/index.html";
+		preg_match($pattern, $subject, $matches);
+
+		//print_r($matches);
+		//http://www.php.net 	all
+		//www.php.net 			only the sub regex.
+		//here, the first parentheses is not contained in the $matches.
+		//
+		
+		$pattern = '#(^[a-zA-Z_.-]+)@([a-zA-Z_]+)\.(?:[a-zA-Z]{2,4}$)#i';
+		$subject = 'jeff@nowhere.com';
+		preg_match($pattern, $subject, $matches);
+
+		//print_r($matches);
 
 	}
 	

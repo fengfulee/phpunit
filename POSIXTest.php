755bf4c6251bcfd0dcefc29b8614788fab7e54e8
VeinQueen <fengfulee@163.com>
2014-07-29 09:54:55 +0800

    POSIX---POSIX-extended Regular Expression.

---
+++ POSIXTest.php
@@ -0,0 +1,18 @@
+<?PHP
+
+/**
+ * POSIX ---POSIX-extended regular expression.
+ * PHP also supports regular expressions using a Perl-compatible syntax using the
+ * PCRE functions. Thosee functons supports non-greedy matching, assertions,
+ * conditional subpatterns, and a number of other features not supported by the 
+ * POSIX-extended regular expression.
+ */
+class POSIXTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Well , function `ereg_replace`, `ereg`, `eregi_replace`, `eregi`, 
+	 * `split`, `spliti`, `sql_regcase` all had been deprecated as of
+	 * PHP 5.3.0.
+	 */
+}
+?>
